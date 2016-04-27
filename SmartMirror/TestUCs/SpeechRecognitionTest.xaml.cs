1bb6ec92d1eaa7b6af1b14eedc92381759432b66
aeglasin <aeglasin@gmx.net>
2016-04-27 12:48:31 +0200

    added GrammarFile as well as structure for Files. edited SpeechRecognition part to be a continius dictation szenario (more closely to our Application)

--- SmartMirror/TestUCs/SpeechRecognitionTest.xaml.cs
+++ SmartMirror/TestUCs/SpeechRecognitionTest.xaml.cs
@@ -14,6 +14,11 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Media.SpeechRecognition;
 using Windows.Media.SpeechSynthesis;
+using Windows.Globalization;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.ApplicationModel;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,51 +29,209 @@ namespace SmartMirror.TestUCs
     /// </summary>
     public sealed partial class SpeechRecoginitionTest : Page
     {
-
+        /// <summary>
+        /// the HResult 0x8004503a typically represents the case where a recognizer for a particular language cannot
+        /// be found. This may occur if the language is installed, but the speech pack for that language is not.
+        /// See Settings -> Time & Language -> Region & Language -> *Language* -> Options -> Speech Language Options.
+        /// </summary>
+        private static uint HResultRecognizerNotFound = 0x8004503a;
         //SpeechSynthesizer _synthesizer;                             // The speech synthesizer (text-to-speech, TTS) object 
-        SpeechRecognizer _recognizer;                               // The speech recognition object 
-        //IAsyncOperation<SpeechRecognitionResult> _recoOperation;    // Used to canel the current asynchronous speech recognition operation 
+        SpeechRecognizer speechRecognizer;                               // The speech recognition object 
+                                                                         //IAsyncOperation<SpeechRecognitionResult> _recoOperation;    // Used to canel the current asynchronous speech recognition operation 
 
         //bool _recoEnabled = false;                                  // When this is true, we will continue to recognize  
+
+        // Speech events may come in on a thread other than the UI thread, keep track of the UI thread's
+        // dispatcher, so we can update the UI in a thread-safe manner.
+        private CoreDispatcher dispatcher;
+
 
         public SpeechRecoginitionTest()
         {
             this.InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
-
-
-           // initSpeechParts();
-            
+            dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+            populateLanguageDropdown();
+            await initRecognizer((Language)((ComboBoxItem)cbLanguageSelection.SelectedItem).Tag);
         }
 
+       private void populateLanguageDropdown()
+        {
+            cbLanguageSelection.SelectionChanged -= cbLanguageSelection_SelectionChanged;
+            Language defaultLanguage = SpeechRecognizer.SystemSpeechLanguage;
+            IEnumerable<Language> supportedLanguages = SpeechRecognizer.SupportedGrammarLanguages;
+            foreach (Language lang in supportedLanguages)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Tag = lang;
+                item.Content = lang.DisplayName;
 
+                cbLanguageSelection.Items.Add(item);
+                if (lang.LanguageTag == defaultLanguage.LanguageTag)
+                {
+                    item.IsSelected = true;
+                    cbLanguageSelection.SelectedItem = item;
+                }
+            }
+            cbLanguageSelection.SelectionChanged += cbLanguageSelection_SelectionChanged;
+        }
+
+        private async void cbLanguageSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await initRecognizer((Language)((ComboBoxItem)cbLanguageSelection.SelectedItem).Tag);
+        }
+
+               /// <summary>
+            /// Initialize Speech Recognizer and compile constraints.
+            /// </summary>
+            /// <param name="recognizerLanguage">Language to use for the speech recognizer</param>
+            /// <returns>Awaitable task.</returns>
+        private async Task initRecognizer(Language recognizedLanguage)
+        {
+           
+            
+            if (speechRecognizer != null)
+            {
+                // cleanup prior to re-initializing this scenario.
+                speechRecognizer.ContinuousRecognitionSession.Completed -= ContinuousRecognitionSession_Completed;
+                speechRecognizer.ContinuousRecognitionSession.ResultGenerated -= ContinuousRecognitionSession_ResultGenerated;
+                speechRecognizer.StateChanged -= SpeechRecognizer_StateChanged;
+
+                this.speechRecognizer.Dispose();
+                this.speechRecognizer = null;
+            }
+
+            try
+            {
+                // Initialize the SRGS-compliant XML file.
+                // For more information about grammars for Windows apps and how to
+                // define and use SRGS-compliant grammars in your app, see
+                // https://msdn.microsoft.com/en-us/library/dn596121.aspx
+
+                // determine the language code being used.
+                string languageTag = recognizedLanguage.LanguageTag;
+                string fileName = String.Format("Assets\\Grammar\\{0}\\Grammar.xml", languageTag);
+                StorageFile grammarContentFile = await Package.Current.InstalledLocation.GetFileAsync(fileName);
+
+                // Initialize the SpeechRecognizer and add the grammar.
+                speechRecognizer = new SpeechRecognizer(recognizedLanguage);
+
+                // Provide feedback to the user about the state of the recognizer. This can be used to provide
+                // visual feedback to help the user understand whether they're being heard.
+                speechRecognizer.StateChanged += SpeechRecognizer_StateChanged;
+
+                SpeechRecognitionGrammarFileConstraint grammarConstraint = new SpeechRecognitionGrammarFileConstraint(grammarContentFile);
+                speechRecognizer.Constraints.Add(grammarConstraint);
+                SpeechRecognitionCompilationResult compilationResult = await speechRecognizer.CompileConstraintsAsync();
+
+                // Check to make sure that the constraints were in a proper format and the recognizer was able to compile them.
+                if (compilationResult.Status != SpeechRecognitionResultStatus.Success)
+                {
+                    //// Disable the recognition button.
+                    //btnContinuousRecognize.IsEnabled = false;
+                    //// Let the user know that the grammar didn't compile properly.
+                    //resultTextBlock.Text = "Unable to compile grammar.";
+                    System.Diagnostics.Debug.WriteLine("Unable to compile grammar, "+compilationResult.Status.ToString());
+                }
+                else
+                {
+
+                    // Set EndSilenceTimeout to give users more time to complete speaking a phrase.
+                    speechRecognizer.Timeouts.EndSilenceTimeout = TimeSpan.FromSeconds(1.2);
+
+                    // Handle continuous recognition events. Completed fires when various error states occur. ResultGenerated fires when
+                    // some recognized phrases occur, or the garbage rule is hit.
+                    speechRecognizer.ContinuousRecognitionSession.Completed += ContinuousRecognitionSession_Completed;
+                    speechRecognizer.ContinuousRecognitionSession.ResultGenerated += ContinuousRecognitionSession_ResultGenerated;
+
+
+                    //btnContinuousRecognize.IsEnabled = true;
+
+                    //resultTextBlock.Text = speechResourceMap.GetValue("SRGSHelpText", speechContext).ValueAsString;
+                    //resultTextBlock.Visibility = Visibility.Visible;
+                }
+            }
+            catch (Exception ex)
+            {
+                if ((uint)ex.HResult == HResultRecognizerNotFound)
+                {
+                    //btnContinuousRecognize.IsEnabled = false;
+
+                    resultTextBlock.Visibility = Visibility.Visible;
+                    resultTextBlock.Text = "Speech Language pack for selected language not installed.";
+                }
+                else
+                {
+                    var messageDialog = new Windows.UI.Popups.MessageDialog(ex.Message, "Exception");
+                    await messageDialog.ShowAsync();
+                }
+            }
+    }
+
+
+
+        //tbd
+        private void SpeechRecognizer_StateChanged(SpeechRecognizer sender, SpeechRecognizerStateChangedEventArgs args)
+        {
+           //Nothing ATM
+        }
+
+        private void ContinuousRecognitionSession_ResultGenerated(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            if (args.Result.SemanticInterpretation.Properties.ContainsKey("FunctionToCall"))
+            {
+                var str = args.Result.SemanticInterpretation.Properties["FunctionToCall"][0].ToString();
+                str = removetokens(str);
+                callTestfunction(str);
+            }
+            else
+                resultTextBlock.Text = args.Result.Text;
+        
+
+
+        }
+ //endTBD
+        private async void ContinuousRecognitionSession_Completed(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionCompletedEventArgs args)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                resultTextBlock.Text = "Recording finished";
+            });
+            
+        }
+       
+
+        /// <summary>
+        /// normal not continius dictation scenario
+        /// </summary>
         private async void initSpeechParts()
         {
             try
             {
-                if (_recognizer == null)
-                {
-                    
 
-                    _recognizer = new SpeechRecognizer();
-                    var lang= _recognizer.CurrentLanguage;
+                if (speechRecognizer == null)
+                {
+
+
+                    speechRecognizer = new SpeechRecognizer();
+                    var lang = speechRecognizer.CurrentLanguage;
                     var functionGrammar = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.Dictation, "Functioncall");
-                    _recognizer.UIOptions.AudiblePrompt = "Give me the name of a Function";
-                    _recognizer.UIOptions.ExampleText = "Function A";
-                    _recognizer.Constraints.Add(functionGrammar) ;
-                    await _recognizer.CompileConstraintsAsync();
+                    speechRecognizer.UIOptions.AudiblePrompt = "Give me the name of a Function";
+                    speechRecognizer.UIOptions.ExampleText = "Function A";
+                    speechRecognizer.Constraints.Add(functionGrammar);
+                    await speechRecognizer.CompileConstraintsAsync();
                 }
-                   
+
                 try
                 {
                     // Start recognition.
 
                     var messageDialog = new Windows.UI.Popups.MessageDialog("Give me a Function to call", "Command me");
                     var dialogAsync = messageDialog.ShowAsync();
-                    SpeechRecognitionResult speechRecognitionResult = await _recognizer.RecognizeAsync();
+                    SpeechRecognitionResult speechRecognitionResult = await speechRecognizer.RecognizeAsync();
                     dialogAsync.Cancel();
                     //seems a bit buggy, need to test a bit more
                     //SpeechRecognitionResult speechRecognitionResult = await _recognizer.RecognizeWithUIAsync();
@@ -113,6 +276,7 @@ namespace SmartMirror.TestUCs
                 case "A":  testFunctionA(); break;
                 case "B":  testFunctionB();break;
                 case "C": testFunctionC();break;
+                case "1": testFunction1();break;
                 default: testFunctionNotAvailable(letter);break;
             }
         }
@@ -130,7 +294,11 @@ namespace SmartMirror.TestUCs
 
         private void testFunctionC()
         {
-            showMessageBox("Function B was called", "Functioncall");
+            showMessageBox("Function C was called", "Functioncall");
+        }
+        private void testFunction1()
+        {
+            showMessageBox("Function 1 was called", "Functioncall");
         }
 
         private void testFunctionNotAvailable(String letter)
@@ -138,11 +306,15 @@ namespace SmartMirror.TestUCs
             showMessageBox("Named function, " + letter + " was not available",  "Functioncall");
         }
         
-        private void showMessageBox(String message,string title)
+        private async void showMessageBox(String message,string title)
         {
-            var messageDialog = new Windows.UI.Popups.MessageDialog(
-           message, "Functioncall");
-            messageDialog.ShowAsync().GetResults();
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+             {
+                 var messageDialog = new Windows.UI.Popups.MessageDialog(
+             message, "Functioncall");
+                 await messageDialog.ShowAsync();
+             });
+           
         }
 
         #endregion
@@ -153,9 +325,49 @@ namespace SmartMirror.TestUCs
 
         #endregion
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private async void button_Click(object sender, RoutedEventArgs e)
         {
-            initSpeechParts();
+            //initSpeechParts();
+
+            button.IsEnabled = false;
+            // The recognizer can only start listening in a continuous fashion if the recognizer is currently idle.
+            // This prevents an exception from occurring.
+            if (speechRecognizer.State == SpeechRecognizerState.Idle)
+            {
+                // Reset the text to prompt the user.
+                try
+                {
+                    await speechRecognizer.ContinuousRecognitionSession.StartAsync();
+                    ButtonText.Text = " Stop Continuous Recognition";
+                    cbLanguageSelection.IsEnabled = false;
+                }
+                catch (Exception ex)
+                {
+                    var messageDialog = new Windows.UI.Popups.MessageDialog(ex.Message, "StartAsync Exception");
+                    await messageDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                try
+                {
+                    // Reset the text to prompt the user.
+                    ButtonText.Text = " Continuous Recognition";
+                    cbLanguageSelection.IsEnabled = true;
+                    // Cancelling recognition prevents any currently recognized speech from
+                    // generating a ResultGenerated event. StopAsync() will allow the final session to 
+                    // complete.
+                    await speechRecognizer.ContinuousRecognitionSession.CancelAsync();
+                }
+                catch (Exception ex)
+                {
+                    var messageDialog = new Windows.UI.Popups.MessageDialog(ex.Message, "CancelAsync Exception");
+                    await messageDialog.ShowAsync();
+                }
+            }
+            button.IsEnabled = true;
         }
+
+      
     }
 }
